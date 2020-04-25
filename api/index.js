c64daedad1898d0ac485f89ca210a7b3a99cb557
adcar <alexanderdcardosi@gmail.com>
2020-04-25 12:39:35 -0400

    Console.log all emails

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
+import base64url from "base64url";
+
 export const listLabels = () => {
   window.gapi.client.gmail.users.labels
     .list({
@@ -17,3 +19,60 @@ export const listLabels = () => {
       }
     });
 };
+
+async function getMessagesRaw(q = "", maxResults = 10) {
+  return await window.gapi.client.gmail.users.messages.list({
+    userId: "me",
+    q,
+    maxResults
+  });
+}
+
+async function getMessage(messageId) {
+  return await window.gapi.client.gmail.users.messages.get({
+    userId: "me",
+    id: messageId,
+    format: "full"
+  });
+}
+
+export async function getMessages() {
+  const res = await getMessagesRaw();
+  for (const { id } of res.result.messages) {
+    const msg = await getMessage(id);
+    console.log(msg.result.payload);
+    let data;
+    if (
+      msg.result.payload.parts !== undefined &&
+      msg.result.payload.parts.length > 0
+    ) {
+      data = msg.result.payload.parts[0].body.data;
+    } else if (msg.result.payload.body.size > 0) {
+      data = msg.result.payload.body.data;
+    }
+
+    console.log(decode(data));
+    // console.log(atob(decode(msg.result.payload.parts[0].body.data)));
+  }
+}
+
+function decode(input) {
+  if (input == undefined) {
+    return null;
+  }
+  // Replace non-url compatible chars with base64 standard chars
+  input = input.replace(/-/g, "+").replace(/_/g, "/");
+
+  // Pad out with standard base64 required padding characters
+  var pad = input.length % 4;
+  if (pad) {
+    if (pad === 1) {
+      throw new Error(
+        "InvalidLengthError: Input base64url string is the wrong length to determine padding"
+      );
+    }
+    input += new Array(5 - pad).join("=");
+  }
+
+  return atob(input);
+}
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from "react";
 import SignOut from "../components/SignOut";
-import { listLabels } from "../api";
+import { getMessages } from "../api";
 export default function dashboard({ token }) {
   useEffect(() => {
-    if (token !== "") {
-      listLabels();
-    }
+    (async () => {
+      if (token !== "") {
+        const res = await getMessages();
+        console.log(res);
+      }
+    })();
   }, [token]);
 
   return (
